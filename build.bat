5c0f10c1f1189822954bccb1aaa1675157af0477
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-11-06 19:08:29 +0000

    Compiler Setup

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 @echo off
-if not exist build mkdir build
+if exist build del "build\*.*?" 
+if exist build echo "Build folder exists deleting old build"
+if not exist build "No build folder found, making new build folder"
+if not exist build mkdir build 
+
 pushd source
 
 make
 
 popd
+
+echo "Moving exe to build folder"
+move "source\*exe*.*" "build"
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd build
-main
+prog
 popd
