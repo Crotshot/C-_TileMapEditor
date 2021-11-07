52d7d1763c9310eb2e58c2b353d7690bd9bd12a7
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-11-07 22:53:03 +0000

    .

--- source/TileMap.hpp
+++ source/TileMap.hpp
@@ -2,27 +2,33 @@
 #include <vector>
 #include <string>
 
-struct MapData
+
+struct mapData
 {
-    MapData(const std::string& tileset, sf::Vector2u tileSize, std::vector<int>& tiles, unsigned int width, unsigned int height):name_(tileset), tileSize_(tileSize),tiles_(tiles),width_(width),height_(height)
+    mapData(const std::string& tileset, sf::Vector2u tileSize, std::vector<int>& tiles, unsigned int width, unsigned int height)
     {
-        
+        name_ = tileset;
+        tileSize_ = tileSize;
+        tiles_ = tiles;
+        width_ = width;
+        height_ = height;
     }
     const std::string name_;
-    sf::Vector2u tileSize_;
+    sf::Vector2u tileSize_; 
     std::vector<int>& tiles_;
     unsigned int width_;
     unsigned int height_;
     
-};
+}Mapdata;
 
 class TileMap: public sf::Drawable, public sf::Transformable
 {
     public:
-    MapData mData;
     TileMap();
-    bool load(MapData mData);
+    
+    bool load(MapData mapDat);
     bool load(const std::string& tileset, sf::Vector2u tileSize, std::vector<int>& tiles, unsigned int width, unsigned int height);
+    MapData mapData;
     
     private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
